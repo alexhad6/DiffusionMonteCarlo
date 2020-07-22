09e383e37f9baca533fe84733dd024b316ac8e1c
alexhad6 <alexhad6@gmail.com>
2020-07-22 02:02:38 -0400

    Finished DMC tests

--- DMCguided.m
+++ DMCguided.m
@@ -1,16 +1,30 @@
-function [E0,Rs] = DMC(D,dims,V,Rs,E0,dt,M0,steps,bSteps,a)
-%DMC Implements the basic diffusion Monte Carlo algorithm.
+function [E0,Rs] = DMCguided(D,dims,PT,EL,F,E0,dt,M0,steps,bSteps,a)
+%DMCGUIDED Implements the guided diffusion Monte Carlo algorithm.
 %   See write-up for detailed explanation and derivation.
 M = M0;                           % Current number of walkers (in block)
+PT2 = @(R) PT(R)^2;               % Trial wave function squared
+mult = 10;                        % Multiple for Metropolis sampling
+sd = 0.1;                         % St. dev. for Metropolis random step
+Rs = zeros(dims,mult*M);          % Allocate array of R values
+i = 1;                            % Index to track current R value
+while i <= mult*M                 % Repeat until enough Rs are collected
+    r = Rs(:,i);                  % Retrieve last accepted R value
+    rp = normrnd(r,sd,dims,1);    % Random step
+    if PT2(rp)/PT2(r) > rand      % Decide whether to accept or reject
+        Rs(:,i+1) = rp;           % If accepted, record R value
+        i = i+1;                  % Increment index
+    end
+end
+Rs = Rs(:,(1:M)*mult);            % Select subset of Rs (sampled from PT2)
 for block = 1:steps/bSteps        % Repeat for the correct number of blocks
     for step = 1:bSteps           % Repeat for each step in a block
         numWalkers = zeros(M,1);  % Tracks number of walker copies
         for i = 1:M               % For each configuration in Rs
             R = Rs(:,i);          % Retrieve R value
-            Rp = R+normrnd(0,sqrt(2*D*dt),dims,1);     % Randomly change R
-            weight = exp(-(dt/2)*(V(Rp)+V(R))+dt*E0);  % Branching weight
-            numWalkers(i) = floor(weight+rand);        % Number of copies
-            Rs(:,i) = Rp;                              % Update R value
+            Rp = R+D*dt*F(R)+normrnd(0,sqrt(2*D*dt),dims,1);  % Step R
+            weight = exp(-(dt/2)*(EL(R)+EL(Rp))+dt*E0);  % Branching weight
+            numWalkers(i) = floor(weight+rand);          % Number of copies
+            Rs(:,i) = Rp;                                % Update R value
         end
         newM = sum(numWalkers);           % New number of walkers
         newRs = zeros(dims,newM);         % Allocate new list of walkers
--- plotDMC.m
+++ plotDMC.m
@@ -1,14 +1,29 @@
-function plotDMC(psi0,R,bins,limits)
-%PLOTDMC Summary of this function goes here
-%   Detailed explanation goes here
-h = histogram(R,bins,'Visible','off');
-centers = (h.BinEdges(1:end-1)+h.BinEdges(2:end))/2;
-scaledValues = h.Values./sqrt(h.BinWidth*sum(h.Values.^2));
+function plotDMC(p0,R,bins,limits,guided,plotfunc)
+%PLOTDMC Plots the DMC distribution against the true wavefunction p0.
+if guided
+    h = histogram(R,bins,'Visible','off','Normalization','pdf');
+    centers = (h.BinEdges(1:end-1)+h.BinEdges(2:end))/2;
+    scaledValues = h.Values;
+else
+    h = histogram(R,bins,'Visible','off','Normalization','pdf');
+    centers = (h.BinEdges(1:end-1)+h.BinEdges(2:end))/2;
+    scaledValues = h.Values./sqrt(h.BinWidth*sum(h.Values.^2));
+end
 xvals = (limits(1):range(limits)/1000:limits(2));
 hold on
 p1 = bar(centers,scaledValues,1,'FaceColor',[0.4,0.6,0.8]);
-p2 = plot(xvals,arrayfun(psi0, xvals),'LineWidth',2);
+if plotfunc
+    p2 = plot(xvals,arrayfun(p0, xvals),'LineWidth',2);
+    legend([p2, p1],'Actual','DMC')
+else
+    legend(p1,'DMC')
+end
 hold off
 xlim(limits)
-legend([p2, p1],'Actual','DMC')
+xlabel('Distance (bohr)')
+if guided
+    ylabel('Probability Density')
+else
+    ylabel('Wave Function')
+end
 end
